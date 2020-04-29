9487ac25acbd63f43fc761032ff0b3f05b9357ab
AJ Kerrigan <aj@speckledmonkey.com>
2020-04-29 03:00:42 -0400

    Switch sendgrid libraries, serve with gunicorn
    Use the 1st party sendgrid library - a separate library for Flask
    is not necessary. Additionally, the community library was failing
    after the initial request:
    
    https://github.com/frankV/flask-sendgrid/issues/19

--- alerts/app.py
+++ alerts/app.py
@@ -1,35 +1,52 @@
 import json
 
 from pathlib import Path
+import textwrap
 
 from flask import Flask, request, jsonify
-from flask_sendgrid import SendGrid
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Email, To, Content, Mail
 
 app = Flask(__name__)
-app.config['SENDGRID_API_KEY'] = Path('/run/secrets/sendgrid_api_key').read_text()
-app.config['SENDGRID_DEFAULT_FROM'] = Path('/run/config/mail_from').read_text()
-mail = SendGrid(app)
+app.config['API_KEY'] = Path('/run/secrets/sendgrid_api_key').read_text()
+app.config['MAIL_FROM'] = Email(Path('/run/config/mail_from').read_text())
+app.config['MAIL_TO'] = To(Path('/run/config/mail_to').read_text())
+
+sg = SendGridAPIClient(api_key=app.config['API_KEY'])
 
 STATUS_MAP = {
     'ok': 'idle',
     'info': 'running'
 }
+MAIL_TEMPLATE = textwrap.dedent('''\
+    Softener status is: {status}
+    
+    Detail:
+    
+    {detail}
+''')
 
 @app.route("/", methods=["POST"])
 def notify():
-    if request.content_type == 'application/json' and '_message' in request.json:
+    if request.content_type == 'application/json' and '_message' in getattr(request, 'json', {}):
         status = STATUS_MAP.get(request.json.get('_level'), 'unknown')
         try:
-            mail.send_email(
-                to_email=[{'email':Path('/run/config/mail_to').read_text()}],
-                subject='Softener Status',
-                text=f'Softener status is: {status}\n\nDetail:\n\n{json.dumps(request.json, indent=4)}'
+            content = Content(
+                'text/plain',
+                MAIL_TEMPLATE.format(status=status, detail=json.dumps(request.json, indent=4))
             )
-            return jsonify(request.json)
+            mail = Mail(
+                app.config['MAIL_FROM'],
+                app.config['MAIL_TO'],
+                'Softener Status',
+                content
+            )
+            response = sg.client.mail.send.post(request_body=mail.get())
+            return response.body, response.status_code
         except Exception as err:
-            print(err)
+            return err
     else:
         return '', 204
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', debug=True, use_reloader=False, use_evalex=False, port=80)
