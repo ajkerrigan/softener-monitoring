d049dde58b38e1ca7beca52d22b8f211b2d44cc3
AJ Kerrigan <aj@speckledmonkey.com>
2020-05-06 00:47:21 -0400

    Avoid multiple notifications
    ...as simply as possible. This should be avoidable from the source,
    but it won't hurt to have a safety valve here.

--- alerts/app.py
+++ alerts/app.py
@@ -3,14 +3,23 @@ import json
 from pathlib import Path
 import textwrap
 
+from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Email, To, Content, Mail
 
 app = Flask(__name__)
-app.config['API_KEY'] = Path('/run/secrets/sendgrid_api_key').read_text()
-app.config['MAIL_FROM'] = Email(Path('/run/config/mail_from').read_text())
-app.config['MAIL_TO'] = To(Path('/run/config/mail_to').read_text())
+app.config.from_mapping({
+    'DEBUG': True,
+    'API_KEY': Path('/run/secrets/sendgrid_api_key').read_text(),
+    'MAIL_FROM': Email(Path('/run/config/mail_from').read_text()),
+    'MAIL_TO': To(Path('/run/config/mail_to').read_text()),
+    'CYCLE_LENGTH': timedelta(hours=3),
+    'CACHE_TYPE': 'simple',
+})
+cache = Cache(app)
+cache.set('last_notification', datetime(1900, 1, 1))
 
 sg = SendGridAPIClient(api_key=app.config['API_KEY'])
 
@@ -28,8 +37,12 @@ MAIL_TEMPLATE = textwrap.dedent('''\
 
 @app.route("/", methods=["POST"])
 def notify():
+    now = datetime.utcnow()
     if request.content_type == 'application/json' and '_message' in getattr(request, 'json', {}):
         status = STATUS_MAP.get(request.json.get('_level'), 'unknown')
+        if cache.get('last_notification') >= (now - app.config['CYCLE_LENGTH']):
+            app.logger.info(f'The last notification was less than a cycle ago ({cache.get("last_notification")})')
+            return 'Too soon'
         try:
             content = Content(
                 'text/plain',
@@ -41,7 +54,9 @@ def notify():
                 'Softener Status',
                 content
             )
+            app.logger.info(f'Sending notification to: {app.config["MAIL_TO"].email}')
             response = sg.client.mail.send.post(request_body=mail.get())
+            cache.set('last_notification', now)
             return response.body, response.status_code
         except Exception as err:
             return err
